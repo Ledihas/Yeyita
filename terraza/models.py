d1a49a4f38b8c0e08f7c703b5410def2ef04856e
zaza <zahidel.pedroso@gmail.com>
2024-07-25 15:21:14 -0400

    se agrega

---
+++ terraza/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class OfrtTerraza(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    presio = models.DecimalField(max_digits=8, decimal_places=2)
+    imagen = models.ImageField(upload_to='ofertasTer/', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.nombre
+    
+class Helados(OfrtTerraza):
+  
+    def __str__(self):
+        return self.nombre
+    
+
+class Bebidas(OfrtTerraza):
+    alcohol = models.DecimalField(max_digits=8, decimal_places=2,max_length=100)
+
+    def __str__(self):
+        return self.nombre
+    
+class Platos(OfrtTerraza):
+
+    def __str__(self):
+        return self.nombre
+    
